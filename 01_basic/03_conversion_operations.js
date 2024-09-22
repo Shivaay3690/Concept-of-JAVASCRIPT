2cddf10af498e5a87fb66b13c178d5f12d106ca3
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-09-22 07:31:50 +0000

    concept of Operators

--- 01_basic/03_conversion_operations.js
+++ 01_basic/03_conversion_operations.js
@@ -24,5 +24,43 @@ let isLoggedIn = "098"
 
  let someNumber = 12
  let stringsomeNumber = String(someNumber)
- console.log(stringsomeNumber)
- console.log(typeof stringsomeNumber)
+//console.log(stringsomeNumber)
+ //console.log(typeof stringsomeNumber)
+
+ // ***************** Operations******************
+
+ let value = 1
+ let negValue = -value
+//  console.log(negValue)
+
+//  console.log(2+3)
+//  console.log(4-2)
+//  console.log(2*4)
+//  console.log(2**3)
+//  console.log(9/2)
+//  console.log(4%2)
+
+let str1 = "Shivam"
+let str2 = "Patel"
+ 
+let str = str1 + str2
+// console.log(str)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(3 + 4 * 5 % 2)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1 ,num2 ,num3
+ num1 = num2 = num3 = 2 + 2
+//  console.log(num2)
+
+ let gameCounter = 100
+ gameCounter++
+ console.log(gameCounter)
