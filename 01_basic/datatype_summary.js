2e2f72d6dd1b54ccd2185a8f699eece386ad5574
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-09-22 08:20:41 +0000

    summary of data types

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,34 @@
+// Primitive Data Types(call by Value in memory stack)
+
+// 7 types: - string boolean number null undefined bigInt symbol
+
+const score = 100
+const valuescoree = 100.4
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail ;
+
+const id = Symbol("1234")
+const anotherId = Symbol("1234")
+
+// console.log(id === anotherId)
+
+const bignumber = 132434312443n
+
+// Non - Primitive Data types
+
+// Array Object function
+
+const heroes = ["shaktiman","naagraj","Doga"];
+
+const myObject = {
+    name: "Shivam",
+    age: 20,
+    bool: true,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof bignumber)
