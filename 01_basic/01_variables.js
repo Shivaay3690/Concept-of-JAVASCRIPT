ddefd1a3357b9b3f5dcbbc4b542e5f91d09bb7fa
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-09-22 07:48:17 +0000

    Avoid comprison

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -10,6 +10,8 @@ accountCity = "Bangluru"
 /*
 Prefer not to use var 
 because of issue in block and functional scope
+const variable:-  ko change nhi kr sakte.fix hota hain
+let variables :- ko redefine kr sakte hain
 */
 console.log(accountId);
 console.table([accountState,accountEmail,accountId,accountPass,accountCity])
---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2 > 4)
+// console.log(8 >= 4)
+// console.log(2 < 4)
+// console.log(2 == 4)
+// console.log(2 != 4)
+// Confusion wala comparision, avoid this type of comparison
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(undefined==0)
+// console.log(undefined > 0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// ===
+
+console.log("2" === 2)
