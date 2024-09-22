558458d59c93b6504028971869e62a25b58d96b1
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-09-22 05:28:58 +0000

    conversion of  data types

---
+++ 01_basic/03_conversion_operations.js
@@ -0,0 +1,28 @@
+// let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+// true => 1
+
+let isLoggedIn = "098"
+ let booleanLoggedIn = Boolean(isLoggedIn)
+
+//  console.log(booleanLoggedIn)
+
+ // 1 => true
+ // 0 => false
+ // "" => false
+ // "Shivam" => true
+
+ let someNumber = 12
+ let stringsomeNumber = String(someNumber)
+ console.log(stringsomeNumber)
+ console.log(typeof stringsomeNumber)
