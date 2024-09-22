f7986a77fb9c7f880a06330e6a88e5fd62586302
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-09-22 05:02:04 +0000

    data types

---
+++ 01_basic/02_data_types.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+// alert(3+2) we are using nodejs , not browser
+
+// console.log(3
+//     +
+//     3);//code readability should be high
+
+// console.log("shivam")
+
+let name = "Shivam"
+let age = 3
+let isloggedIn = false
+let state;
+// let state = null;
+
+// Primitive data types
+
+// number => 2 to power 53
+// bigInt 
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// console.log(typeof undefined) // undefined
+// console.log(typeof null);// object
