c785adcddae3c5f20e6eedd7e5f83c78249eea7e
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-09-23 11:24:58 +0000

    concept of math and number

---
+++ 01_basic/06_num_math.js
@@ -0,0 +1,38 @@
+const score = 1000
+// console.log(score);
+
+const score1 = new Number(100)
+// console.log(score1);
+
+// console.log(score1.toString().length);
+// console.log(score1.toFixed());
+
+
+const num = 1123.8866
+// console.log(num.toPrecision(3));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString("en-IN"));
+
+// ++++++++++++++++ Maths +++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(9.7));
+// console.log(Math.floor(5.9));
+//console.log(Math.ceil(5.1));//ceil method me 5.something hoga to hmesha +1 ho kr answer milega
+// console.log(Math.max(3,1,6,4));
+// console.log(Math.min(3,1,6,4));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log((Math.floor(Math.random()*10) + 1 ));
+// console.log(Math.ceil(Math.random()*10));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min);
+
+
+
