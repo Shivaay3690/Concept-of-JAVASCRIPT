6d9a046036efd130fc9dc38c56ac2973655691ab
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-09-24 08:51:26 +0000

    basic of arrays

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,40 @@
+myArr = [2,4,1,5,6]
+// console.log(myArr[0]);
+// console.log(myArr);
+
+const number = new Array(8,2,5,2,8)
+// console.log(number);
+
+
+//*******Array Methods **********
+// myArr.push(8)
+// myArr.push(0)
+
+// console.log(myArr)
+// myArr.pop()
+
+// console.log(myArr)
+
+//  myArr.unshift(5)
+//  myArr.shift()
+
+//  console.log(myArr.includes(3))
+//  console.log(myArr.indexOf(1))
+
+// const newAarray = myArr.join()
+
+// console.log(newAarray);
+// console.log(typeof newAarray);
+
+// Slice and splice
+
+console.log("A ",myArr)
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+
+console.log("B ",myArr);
+const myn2 = myArr.splice(1,3)
+
+console.log("C ",myArr);
+console.log(myn2);
+
