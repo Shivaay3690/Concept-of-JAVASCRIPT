7c6157e934f65e3dcf183f8e09add3e648a0e2b8
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-09-27 09:46:03 +0000

    all about this keyword and arrow function

---
+++ 03_basic/03_arrow.js
@@ -0,0 +1,54 @@
+const user = {
+    username : "Shivam",
+    price : 999,
+    WlcmMessage : function(){
+        // console.log(`${this.username},Welcome to the website`);
+        // console.log(this);
+        
+    }
+}
+user.WlcmMessage()
+user.username = "Shivendra"
+// user.WlcmMessage()
+// console.log(this);
+
+
+// function chai(){
+//     console.log(this);
+    
+// }
+// chai()
+
+// function chai(){
+//     let username = "Shivam"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "Shivendra"
+//     console.log(this.username);
+    
+// }
+ 
+// const chai = () => {
+//     let username = "Shivendra"
+//     //console.log(this.username);
+//    // console.log(this);// interview queestion
+    
+    
+// }
+
+// chai()
+
+// const addTwo = (num1 , num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => ( num1 + num2)
+
+// const addTwo = (num1,num2) => ({username: "Shivam"})
+// console.log(addTwo(2,3))
+
+
