f7b58aeb135f90373f9901be7f944af68917cacd
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-09-29 12:04:53 +0000

    concept of iife

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,10 @@
+// Immedialtely  invoked function Expression ( iife)
+
+(function chai(){
+    console.log(`DB CONNECTED`)
+}) ();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO , ${name}`);
+    
+}) ("Shivam")
