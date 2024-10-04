18bc4247a92aa9970df635301f87883a3ac5f3f6
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-10-04 07:58:50 +0000

    html file of bmi calculator

--- 07_project/02_bmi_calculator/logic.js
+++ 07_project/02_bmi_calculator/logic.js
@@ -21,5 +21,6 @@ form.addEventListener('submit', function (e) {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
         //show the result
         results.innerHTML = `<span>${bmi}</span>`;
+        
     }
 });
